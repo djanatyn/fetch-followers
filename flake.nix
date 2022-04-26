90655c24f40cae8df1228854aab02e067955651e
Jonathan Strickland <djanatyn@gmail.com>
2022-04-26 14:48:35 -0400

    switch to makeRustPlatform
    information taken from:
    - https://www.breakds.org/post/build-rust-package/
    - https://github.com/mozilla/nixpkgs-mozilla#using-in-nix-expressions

--- flake.nix
+++ flake.nix
@@ -4,20 +4,31 @@
   inputs = { mozilla.url = "github:mozilla/nixpkgs-mozilla"; };
 
   outputs = { self, nixpkgs, mozilla }: {
-    packages.x86_64-linux.fetch-followers =
-      let pkgs = import nixpkgs { overlays = [ mozilla.overlays.rust ]; };
-      in pkgs.rustPlatform.buildRustPackage rec {
-        pname = "fetch-followers";
-        version = "1.0";
-        src = ./.;
-        cargoSha256 = "sha256-vw4YeBub9DwXRN0YEfIelzc2NEC+nr3cZano+7guzj0=";
-        cargoDepsName = pname;
-        buildInputs = [ pkgs.openssl ];
-        nativeBuildInputs = with pkgs; [
-          protobuf
-          pkg-config
-          latest.rustChannels.nightly.rust
-        ];
+    packages.x86_64-linux.fetch-followers = let
+      pkgs = import nixpkgs { overlays = [ mozilla.overlays.rust ]; };
+
+      nightly = (pkgs.rustChannelOf {
+        date = "2022-04-12";
+        channel = "nightly";
+      }).rust;
+
+      platform = pkgs.makeRustPlatform {
+        cargo = nightly;
+        rustc = nightly;
       };
+    in platform.buildRustPackage rec {
+      pname = "fetch-followers";
+      version = "1.0";
+      buildInputs = with pkgs; [ openssl ];
+      nativeBuildInputs = with pkgs; [
+        protobuf
+        pkg-config
+        latest.rustChannels.nightly.rust
+      ];
+
+      cargoSha256 = "sha256-pI8FgvyqYVsl2muPSxo2cD+8ysMRkacNECNDPbQUGA0=";
+
+      src = ./.;
+    };
   };
 }
