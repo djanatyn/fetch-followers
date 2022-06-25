a427e9ace9c12945b4d736a887729b925a2f6993
Jonathan Strickland <djanatyn@gmail.com>
2022-06-24 20:22:01 -0400

    mozilla/nixpkgs-mozilla -> oxalica/rust-overlay

--- flake.nix
+++ flake.nix
@@ -1,33 +1,24 @@
 {
   description = "flake for fetch-followers";
 
-  inputs = { mozilla.url = "github:mozilla/nixpkgs-mozilla"; };
+  inputs = { rust-overlay.url = "github:oxalica/rust-overlay"; };
 
-  outputs = { self, nixpkgs, mozilla }: {
-    packages.x86_64-linux.fetch-followers = let
-      pkgs = import nixpkgs { overlays = [ mozilla.overlays.rust ]; };
+  outputs = { self, nixpkgs, rust-overlay }: {
+    packages.x86_64-linux.fetch-followers =
+      let pkgs = import nixpkgs { overlays = [ rust-overlay.overlay ]; };
 
-      nightly = (pkgs.rustChannelOf {
-        date = "2022-04-12";
-        channel = "nightly";
-      }).rust;
+      in pkgs.rustPlatform.buildRustPackage rec {
+        pname = "fetch-followers";
+        version = "1.0";
+        buildInputs = with pkgs; [ openssl sqlite ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          latest.rustChannels.nightly.rust
+        ];
 
-      platform = pkgs.makeRustPlatform {
-        cargo = nightly;
-        rustc = nightly;
+        cargoSha256 = "sha256-EByARHVCJR8H8gtVHtNsRpcT1AyEf37KfKyGmYpUSgM=";
+
+        src = ./.;
       };
-    in platform.buildRustPackage rec {
-      pname = "fetch-followers";
-      version = "1.0";
-      buildInputs = with pkgs; [ openssl sqlite ];
-      nativeBuildInputs = with pkgs; [
-        pkg-config
-        latest.rustChannels.nightly.rust
-      ];
-
-      cargoSha256 = "sha256-EByARHVCJR8H8gtVHtNsRpcT1AyEf37KfKyGmYpUSgM=";
-
-      src = ./.;
-    };
   };
 }
